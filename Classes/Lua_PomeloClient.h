76662076cd8f8d8bdafa895cb2b3dc8424fa50a5
qianbinfeng <kevin.qian@msn.com>
2014-04-04 11:46:14 +0800

    Client Code in C++

---
+++ Classes/Lua_PomeloClient.h
@@ -0,0 +1,14 @@
+/*
+** Lua binding: PomeloClient
+** Generated automatically by tolua++-1.0.92 on Wed Apr  2 18:23:35 2014.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "tolua++.h"
+#ifdef __cplusplus
+}
+#endif
+/* Exported function */
+TOLUA_API int  tolua_PomeloClient_open (lua_State* tolua_S);
+
